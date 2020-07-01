b8400382060d8aaab2ed2dcd151ea6f6abff5f45
keddo <adu.kedir.abdurahman@gmail.com>
2020-07-01 11:30:54 +0300

    New ES6 syntax: use let instead of var

--- block_let_const.js
+++ block_let_const.js
@@ -1,8 +1,21 @@
 'use strict'
 function updateProductId() {
-    productId = 12;
+    productId = 12; // temporal dead zone
 }
 
 let productId = null;
 updateProductId();
 console.log(productId);
+
+let updateFunctions = [];
+for (var i = 0; i < 2; i++) {
+    updateFunctions.push(function () { return i });
+}
+// console.log(updateFunctions);
+console.log(updateFunctions[0]()); // output 2
+
+let updateFunctions = [];
+for (let i = 0; i < 2; i++) {
+    updateFunctions.push(function () { return i });
+}
+console.log(updateFunctions[0]()); // output 0
