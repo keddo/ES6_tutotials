fbc95de65aa434aa8d5a1f64a2e38f3e38547112
keddo <adu.kedir.abdurahman@gmail.com>
2020-07-01 18:17:53 +0300

    Modules and Classes: static members

---
+++ static_class_members.js
@@ -0,0 +1,11 @@
+class Project {
+    static getDefaultId() {
+        return 0;
+    }
+}
+
+
+console.log(Project.getDefaultId());
+// let p = new Project();
+// console.log(p.getDefaultId()); // TypeError: p.getDefaultId is not a function
+
