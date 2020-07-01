3fc248f2eaf755f0fc4e7ea7b1ed178928de6d11
keddo <adu.kedir.abdurahman@gmail.com>
2020-07-01 16:57:09 +0300

    Classes and Modules: Module basics

---
+++ base.js
@@ -0,0 +1,5 @@
+console.log('starting in base');
+// let { projectName, projectId } = require('./modules');
+let { projectId } = require('./modules');
+console.log(`has id: ${projectId}`);
+console.log(`ending in base`);
---
+++ modules.js
@@ -0,0 +1,7 @@
+let projectId = 99;
+let projectName = 'BuiltIt';
+
+// module.exports = { projectId, projectName };
+exports.projectId = projectId; // default export
+console.log('in module');
+
