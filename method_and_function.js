78cbdb4660ca3bee7397493731554b40d6d6b909
keddo <adu.kedir.abdurahman@gmail.com>
2020-07-19 14:16:36 +0300

    Difference between function and method.

---
+++ method_and_function.js
@@ -0,0 +1,11 @@
+const name = 'shaun';
+
+// function
+const greet = () => 'Hello';
+
+console.log(greet()); // functions are directly called 
+
+
+
+// method
+console.log(name.toUpperCase()); // methods are used with dot notation
